8f47f122771b47412063781a397c392182534350
Kaltham <k_alkhuzaei@hotmail.com>
2017-10-20 20:44:06 +0300

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :products
+  resources :order_items
+  resource :carts, only: [:show]
+  get '/receipt' => "pages#receipt"
+  
+  root "home#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

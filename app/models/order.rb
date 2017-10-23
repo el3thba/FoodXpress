cf0345d07f7dba463ed6a7887adcefcc5befbc4b
Kaltham <k_alkhuzaei@hotmail.com>
2017-10-23 14:48:24 +0300

    second commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,18 @@
 class Order < ApplicationRecord
 	has_many:order_items
+	has_many:product
+
 
 	def subtotal
 		order_items.collect {|order_item| order_item.valid? ? (order_item.unit_price*order_item.quantity) : 0}.sum
 	end
+
+	def discount
+		print "*"
+		self[:discount] = TotalPrice *(TotalPrice-discount)*100
+		print discount
+	end
+
 
 		private
 		def set_subtotal

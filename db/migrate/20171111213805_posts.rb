7e095a23159fc8f7c99be252b9902a213aea6113
el3thba <el3thba@me.com>
2017-12-03 01:27:01 +0300

    fin.

--- db/migrate/20171111213805_posts.rb
+++ db/migrate/20171111213805_posts.rb
@@ -1,9 +1,9 @@
 class Posts < ActiveRecord::Migration[5.1]
 
-    # def change
-  	#   	create_table :posts do |t|
-    #  	 	t.text :title
-    #  		t.text :text
-    #  	end
-    # end
+    def change
+  	  	create_table :posts do |t|
+     	 	t.string :title
+     		t.text :text
+     	end
+    end
 end

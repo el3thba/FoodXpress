a269d6a27e0edc10198e99862fb883aaa3a09e82
el3thba <el3thba@me.com>
2017-12-02 23:29:25 +0300

    finale

--- db/migrate/20171111213805_posts.rb
+++ db/migrate/20171111213805_posts.rb
@@ -4,6 +4,7 @@ class Posts < ActiveRecord::Migration[5.1]
   	  	create_table :posts do |t|
      	 	t.string :title
      		t.text :text
+        t.timestamps
      	end
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20171201120715) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "rating"
   end
 

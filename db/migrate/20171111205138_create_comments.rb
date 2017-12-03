c60ab7d949a0dc21107720354696fa295536f516
el3thba <el3thba@me.com>
2017-12-03 10:17:53 +0300

    test

--- db/migrate/20171111205138_create_comments.rb
+++ db/migrate/20171111205138_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.string :name
       t.text :body
-      t.references :post, index: true, foreign_key: true
+      # t.references :post, index: true, foreign_key: true
 
       t.timestamps
     end
